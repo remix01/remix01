91c7e5988ce2729e835b0189b9b2f50bbe5a7504
remin rekovic <info.remi@me.com>
2026-03-24 10:07:58 +0100

    feat(ui): Add loading skeleton for narocnik sporocila

---
+++ app/(narocnik)/sporocila/loading.tsx
@@ -0,0 +1,39 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function SporocilaLoading() {
+  return (
+    <div className="flex h-[calc(100vh-4rem)]">
+      {/* Conversation List */}
+      <div className="w-80 border-r p-4 space-y-3">
+        <Skeleton className="h-10 w-full mb-4" />
+        {[1, 2, 3, 4, 5].map((i) => (
+          <div key={i} className="flex items-center gap-3 p-3">
+            <Skeleton className="h-10 w-10 rounded-full" />
+            <div className="flex-1">
+              <Skeleton className="h-4 w-32 mb-1" />
+              <Skeleton className="h-3 w-48" />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Chat Area */}
+      <div className="flex-1 flex flex-col">
+        <div className="border-b p-4 flex items-center gap-3">
+          <Skeleton className="h-10 w-10 rounded-full" />
+          <Skeleton className="h-5 w-40" />
+        </div>
+        <div className="flex-1 p-4 space-y-4">
+          {[1, 2, 3].map((i) => (
+            <div key={i} className={i % 2 === 0 ? "flex justify-end" : ""}>
+              <Skeleton className={`h-16 ${i % 2 === 0 ? "w-2/3" : "w-1/2"} rounded-lg`} />
+            </div>
+          ))}
+        </div>
+        <div className="border-t p-4">
+          <Skeleton className="h-12 w-full" />
+        </div>
+      </div>
+    </div>
+  )
+}
