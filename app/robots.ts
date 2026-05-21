42b1f844cc3b38608f87744fb7bb36d0d4037cdf
remin rekovic <info.remi@me.com>
2026-05-21 11:56:35 +0200

    Allow crawling legacy jobs URLs for redirect discovery

--- app/robots.ts
+++ app/robots.ts
@@ -18,7 +18,6 @@ export default function robots(): MetadataRoute.Robots {
           '/profil/',
           '/obvestila/',
           '/povprasevanja/',
-          '/jobs/',
           '/novo-povprasevanje/',
           '/ocena/',
           '/sandbox',
